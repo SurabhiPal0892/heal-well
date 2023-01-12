5aabfb21c1ee0c1fded22bb97fb672bb90f6fbb8
Surabhi Pal <surpal@WKMZTC9B2119.global.publicisgroupe.net>
2023-01-12 14:10:08 +0530

    initial push

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Header(){
+    return(
+        <header>
+        <p>HEAL WELL PHYSIOTHERAPY</p>
+        <i className="fa fa-phone" aria-hidden="true"></i>
+
+        <span>Send us a message</span>
+        <span>Timings</span>
+
+        </header>
+    )
+}
+
---
+++ src/components/nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function Nav(){
+    return(
+        <nav>
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT US</li>
+            <li>WHAT WE OFFER</li>
+            <li>SERVICES</li>
+            <li>CONTACT US</li>
+            <li>BOOK AN APPOINTMENT</li>
+        </ul>
+        </nav>
+    )
+}
+
