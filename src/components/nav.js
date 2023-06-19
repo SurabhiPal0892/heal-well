dc6a35dca40124cf538e159f8d7d156f2b237cf8
SurabhiPal0892 <palsurabhi.0892@gmail.com>
2023-06-19 23:19:23 +0530

    nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,7 +6,6 @@ export function NavComponent() {
             <Link to="/"><li>HOME</li></Link>
             <Link to="/about-us"><li>ABOUT US</li></Link>
             <Link to="/what-we-offer">  <li>WHAT WE OFFER</li></Link>
-            <Link to="/services"><li>SERVICES</li></Link>
             <Link to="/contact-us"><li>CONTACT US</li></Link>
             <Link to="/book-appointment"><li>BOOK AN APPOINTMENT</li></Link>
         </ul>
